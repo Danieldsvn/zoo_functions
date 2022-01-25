d138675c01b6899c975bbc69f09197546562e84c
Daniel Saraiva <danieldsvn@gmail.com>
2022-01-25 18:15:14 -0300

    Requisito 3 concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,15 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const getFirst = data.employees.filter((first) => first.firstName === employeeName);
+  if (getFirst.length === 1) {
+    return getFirst[0];
+  }
+  const getLast = data.employees.filter((last) => last.lastName === employeeName);
+  return getLast[0];
 }
 
 module.exports = getEmployeeByName;
