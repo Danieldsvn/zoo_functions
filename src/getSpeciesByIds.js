8ccd685221eaefff619d8ade4b568f6704dedf43
Daniel Saraiva <danieldsvn@gmail.com>
2022-01-25 16:16:38 -0300

    Requisito 1 2/3 dos testes passaram

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(id) {
   // seu código aqui
+  if (id === undefined) {
+    return [];
+  }
+  const idFiltred = data.species.filter((specie) => specie.id === id);
+  return idFiltred;
 }
-
+console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
