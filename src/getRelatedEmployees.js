4e77c40d848b2cc6cf544f30dd673cf1d1e83b9f
Daniel Saraiva <danieldsvn@gmail.com>
2022-01-26 17:26:42 -0300

    Refatorando a funçao 'isManager' do requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,19 +1,15 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
-  const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
-  const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
-  const managers = [stephanieId, olaId, burlId];
-  const result = managers.some((manager) => manager === id);
-  return result;
+  const managers = data.employees.some((manager) => manager.managers.includes(id));
+  return managers;
 }
 
 function getRelatedEmployees(managerId) {
   if (isManager(managerId) === false) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
-  const managerRelated = data.employees.find((manager) => manager.managers === managerId);
+  const managerRelated = data.employees.filter((manager) => manager.managers === managerId);
   return managerRelated;
 }
 
