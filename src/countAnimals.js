d2106ebcdaed09c5ecbdda9b84eaa4878ac06cb7
Daniel Saraiva <danieldsvn@gmail.com>
2022-01-26 16:21:26 -0300

    Ajustes do lint requisito 1 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,13 +1,7 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
-  if (animal === undefined) {
-    // const catchAllAnimals = (data.species.name): data.species.popularity 
-    const catchAnimals = data.species.forEach((line) => {
-      
-    })
-  }
+// seu código aqui
 }
 
 module.exports = countAnimals;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,13 +4,8 @@ function getSpeciesByIds(...id) {
   // seu código aqui
   if (id === undefined) {
     return [];
-  }  
-  // const testEachId = ids.forEach((id) => {
-  //   id
-  // })
+  }
   const idFiltred = data.species.filter((specie) => id.includes(specie.id));
   return idFiltred;
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
-'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 module.exports = getSpeciesByIds;
