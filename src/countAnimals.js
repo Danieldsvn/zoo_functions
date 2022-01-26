87cc0c266e204428128eaa67b71575bdf8da6827
Daniel Saraiva <danieldsvn@gmail.com>
2022-01-26 16:17:32 -0300

    Requisito 1 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    // const catchAllAnimals = (data.species.name): data.species.popularity 
+    const catchAnimals = data.species.forEach((line) => {
+      
+    })
+  }
 }
 
 module.exports = countAnimals;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,12 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(id) {
+function getSpeciesByIds(...id) {
   // seu código aqui
   if (id === undefined) {
     return [];
-  }
-  const idFiltred = data.species.filter((specie) => specie.id === id);
+  }  
+  // const testEachId = ids.forEach((id) => {
+  //   id
+  // })
+  const idFiltred = data.species.filter((specie) => id.includes(specie.id));
   return idFiltred;
 }
-// console.log(getSpeciesByIds());
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
+'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 module.exports = getSpeciesByIds;
