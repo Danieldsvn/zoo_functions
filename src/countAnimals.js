4e620db9aa5580fa71c6530751e2ad835e77da01
Daniel Saraiva <danieldsvn@gmail.com>
2022-01-27 19:43:17 -0300

    Requisito 5 2/3 de testes concluidos

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-// seu código aqui
+  if (animal === undefined) {
+    
+  } 
+  if (Object.keys(animal).length > 0) {
+    const species = data.species.find((specieFilted) => specieFilted.name === animal.specie);    
+    if (Object.keys(animal).length === 1) { // Entrando apenas com a chave specie
+      const speciesAmount = species.residents.length;
+      return speciesAmount;
+    } 
+    else { // Entrando com a chave specie e a chave sex
+      let animalSexCounter = 0;      
+      const sexSpecies = species.residents.forEach((properties) => {
+        if (properties.sex === animal.sex) {
+          animalSexCounter += 1;    
+        }
+        return animalSexCounter;
+      }); 
+      return animalSexCounter;
+    }
+  } 
 }
 
 module.exports = countAnimals;
