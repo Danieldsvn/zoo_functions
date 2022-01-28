c5c0183bd9b7386eb25bcb0fbb29623d7de80e95
Daniel Saraiva <danieldsvn@gmail.com>
2022-01-27 22:08:00 -0300

    Requisito 5 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,25 +2,23 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   if (animal === undefined) {
-    
-  } 
-  if (Object.keys(animal).length > 0) {
-    const species = data.species.find((specieFilted) => specieFilted.name === animal.specie);    
-    if (Object.keys(animal).length === 1) { // Entrando apenas com a chave specie
-      const speciesAmount = species.residents.length;
-      return speciesAmount;
-    } 
-    else { // Entrando com a chave specie e a chave sex
-      let animalSexCounter = 0;      
-      const sexSpecies = species.residents.forEach((properties) => {
-        if (properties.sex === animal.sex) {
-          animalSexCounter += 1;    
-        }
-        return animalSexCounter;
-      }); 
-      return animalSexCounter;
+    const allSpeciesAmount = data.species.reduce((accumulator, currentValue) => {
+      accumulator[currentValue.name] = currentValue.residents.length;
+      return accumulator;
+    }, {});
+    return allSpeciesAmount;
+  }
+  const species = data.species.find((specieFilted) => specieFilted.name === animal.specie);
+  if (Object.keys(animal).length === 1) { // Entrando apenas com a chave specie
+    return species.residents.length;
+  }
+  let animalSexCounter = 0; // Entrando com a chave specie e a chave sex
+  species.residents.forEach((properties) => {
+    if (properties.sex === animal.sex) {
+      animalSexCounter += 1;
     }
-  } 
+    return animalSexCounter;
+  });
 }
 
 module.exports = countAnimals;
