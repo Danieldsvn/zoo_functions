023976d6c895975e5fc58f7fa22c821507be5323
Daniel Saraiva <danieldsvn@gmail.com>
2022-01-30 21:24:46 -0300

    Requisito 9 concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,25 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function getFirstAnimalId(personId) {
+  const getEmployeeObject = employees.find((employee) => (employee.id === personId));
+  const getFirstAnimal = getEmployeeObject.responsibleFor[0];
+  return getFirstAnimal;
+}
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalId = getFirstAnimalId(id);
+  const animalObject = data.species.find((aniId) => (aniId.id === animalId));
+  const sortedByAge = animalObject.residents.sort((a, b) => {
+    if (a.age < b.age) {
+      return -1;
+    }
+    if (a.age > b.age) {
+      return 1;
+    }
+    return 0;
+  });
+  const oldest = sortedByAge[sortedByAge.length - 1];
+  return [oldest.name, oldest.sex, oldest.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+function animalSchedule(animal) { // Parte 4
+  const animals = data.species.find((animalFinded) => animalFinded.name === animal);
+  if (animals === undefined) {
+    return undefined;
+  }
+  return animals.availability;
 }
+
+function getSchedule(scheduleTarget) {
+  // if (scheduleTarget === undefined) { // Parte 1
+  // }
+  if (animalSchedule(scheduleTarget) !== undefined) {
+    return animalSchedule(scheduleTarget);
+  }
+  return 'lindão';
+}
+
+// function zooClosed(day) {
+// }
+
+console.log(getSchedule('lions'));
+// console.log(getSchedule());
 
 module.exports = getSchedule;
